e6d641a9cf03f60bd0d7fc80f271a5bc42649cfe
Intezar ahmad <128510767+Intezar-ahmad@users.noreply.github.com>
2023-06-27 18:16:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnElement = document.getElementById("mouseOver");
+
+btnElement.addEventListener("mouseover", function(){
+    document.body.style.color = "red"
+    alert("You just mouseOver h1 ☺")
+})
